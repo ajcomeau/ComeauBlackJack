5ad060c2274284f9863eac0ea4339d11fe462587
ajcomeau <acomeau@drewslair.com>
2018-06-07 21:24:13 -0400

    Gameplay mostly complete. Bank operational.

--- ComeauBlackJack/ComeauBlackJack/BlackJackTable.Designer.cs
+++ ComeauBlackJack/ComeauBlackJack/BlackJackTable.Designer.cs
@@ -37,6 +37,8 @@
             this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.gameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.dealToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.standToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.btnLoadPlayer = new System.Windows.Forms.Button();
             this.lblPlayerName = new System.Windows.Forms.Label();
             this.lblScore = new System.Windows.Forms.Label();
@@ -47,6 +49,10 @@
             this.lblPlayer = new System.Windows.Forms.Label();
             this.lblDealerHandValue = new System.Windows.Forms.Label();
             this.lblPlayerHandValue = new System.Windows.Forms.Label();
+            this.btnHit = new System.Windows.Forms.Button();
+            this.btnStand = new System.Windows.Forms.Button();
+            this.txtBet = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -148,7 +154,9 @@
             // gameToolStripMenuItem
             // 
             this.gameToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.dealToolStripMenuItem});
+            this.dealToolStripMenuItem,
+            this.hitToolStripMenuItem,
+            this.standToolStripMenuItem});
             this.gameToolStripMenuItem.Name = "gameToolStripMenuItem";
             this.gameToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
             this.gameToolStripMenuItem.Text = "&Game";
@@ -156,13 +164,27 @@
             // dealToolStripMenuItem
             // 
             this.dealToolStripMenuItem.Name = "dealToolStripMenuItem";
-            this.dealToolStripMenuItem.Size = new System.Drawing.Size(97, 22);
+            this.dealToolStripMenuItem.Size = new System.Drawing.Size(104, 22);
             this.dealToolStripMenuItem.Text = "&Deal";
             this.dealToolStripMenuItem.Click += new System.EventHandler(this.DealHand_Click);
             // 
+            // hitToolStripMenuItem
+            // 
+            this.hitToolStripMenuItem.Name = "hitToolStripMenuItem";
+            this.hitToolStripMenuItem.Size = new System.Drawing.Size(104, 22);
+            this.hitToolStripMenuItem.Text = "&Hit";
+            this.hitToolStripMenuItem.Click += new System.EventHandler(this.HitButton_Click);
+            // 
+            // standToolStripMenuItem
+            // 
+            this.standToolStripMenuItem.Name = "standToolStripMenuItem";
+            this.standToolStripMenuItem.Size = new System.Drawing.Size(104, 22);
+            this.standToolStripMenuItem.Text = "&Stand";
+            this.standToolStripMenuItem.Click += new System.EventHandler(this.StandButton_Click);
+            // 
             // btnLoadPlayer
             // 
-            this.btnLoadPlayer.Location = new System.Drawing.Point(532, 233);
+            this.btnLoadPlayer.Location = new System.Drawing.Point(540, 79);
             this.btnLoadPlayer.Name = "btnLoadPlayer";
             this.btnLoadPlayer.Size = new System.Drawing.Size(95, 23);
             this.btnLoadPlayer.TabIndex = 2;
@@ -192,7 +214,7 @@
             // 
             // panelPlayer
             // 
-            this.panelPlayer.Location = new System.Drawing.Point(75, 233);
+            this.panelPlayer.Location = new System.Drawing.Point(75, 242);
             this.panelPlayer.Name = "panelPlayer";
             this.panelPlayer.Size = new System.Drawing.Size(451, 145);
             this.panelPlayer.TabIndex = 5;
@@ -206,7 +228,7 @@
             // 
             // btnDeal
             // 
-            this.btnDeal.Location = new System.Drawing.Point(208, 33);
+            this.btnDeal.Location = new System.Drawing.Point(451, 36);
             this.btnDeal.Name = "btnDeal";
             this.btnDeal.Size = new System.Drawing.Size(75, 23);
             this.btnDeal.TabIndex = 7;
@@ -220,15 +242,15 @@
             this.lblDealer.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblDealer.Location = new System.Drawing.Point(71, 39);
             this.lblDealer.Name = "lblDealer";
-            this.lblDealer.Size = new System.Drawing.Size(56, 20);
+            this.lblDealer.Size = new System.Drawing.Size(60, 20);
             this.lblDealer.TabIndex = 8;
-            this.lblDealer.Text = "Dealer";
+            this.lblDealer.Text = "Dealer:";
             // 
             // lblPlayer
             // 
             this.lblPlayer.AutoSize = true;
             this.lblPlayer.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblPlayer.Location = new System.Drawing.Point(71, 210);
+            this.lblPlayer.Location = new System.Drawing.Point(71, 218);
             this.lblPlayer.Name = "lblPlayer";
             this.lblPlayer.Size = new System.Drawing.Size(62, 20);
             this.lblPlayer.TabIndex = 9;
@@ -238,25 +260,68 @@
             // 
             this.lblDealerHandValue.AutoSize = true;
             this.lblDealerHandValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblDealerHandValue.Location = new System.Drawing.Point(531, 183);
+            this.lblDealerHandValue.Location = new System.Drawing.Point(133, 36);
             this.lblDealerHandValue.Name = "lblDealerHandValue";
-            this.lblDealerHandValue.Size = new System.Drawing.Size(0, 24);
+            this.lblDealerHandValue.Size = new System.Drawing.Size(22, 24);
             this.lblDealerHandValue.TabIndex = 10;
+            this.lblDealerHandValue.Tag = "test";
+            this.lblDealerHandValue.Text = "--";
             // 
             // lblPlayerHandValue
             // 
             this.lblPlayerHandValue.AutoSize = true;
             this.lblPlayerHandValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblPlayerHandValue.Location = new System.Drawing.Point(532, 354);
+            this.lblPlayerHandValue.Location = new System.Drawing.Point(139, 215);
             this.lblPlayerHandValue.Name = "lblPlayerHandValue";
-            this.lblPlayerHandValue.Size = new System.Drawing.Size(0, 24);
+            this.lblPlayerHandValue.Size = new System.Drawing.Size(22, 24);
             this.lblPlayerHandValue.TabIndex = 11;
+            this.lblPlayerHandValue.Text = "--";
+            // 
+            // btnHit
+            // 
+            this.btnHit.Location = new System.Drawing.Point(532, 291);
+            this.btnHit.Name = "btnHit";
+            this.btnHit.Size = new System.Drawing.Size(95, 23);
+            this.btnHit.TabIndex = 12;
+            this.btnHit.Text = "Hit";
+            this.btnHit.UseVisualStyleBackColor = true;
+            this.btnHit.Click += new System.EventHandler(this.HitButton_Click);
+            // 
+            // btnStand
+            // 
+            this.btnStand.Location = new System.Drawing.Point(532, 320);
+            this.btnStand.Name = "btnStand";
+            this.btnStand.Size = new System.Drawing.Size(95, 23);
+            this.btnStand.TabIndex = 13;
+            this.btnStand.Text = "Stand";
+            this.btnStand.UseVisualStyleBackColor = true;
+            this.btnStand.Click += new System.EventHandler(this.StandButton_Click);
+            // 
+            // txtBet
+            // 
+            this.txtBet.Location = new System.Drawing.Point(366, 38);
+            this.txtBet.Name = "txtBet";
+            this.txtBet.Size = new System.Drawing.Size(79, 20);
+            this.txtBet.TabIndex = 14;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(334, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 13);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Bet:";
             // 
             // BlackJackTable
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(725, 411);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBet);
+            this.Controls.Add(this.btnStand);
+            this.Controls.Add(this.btnHit);
             this.Controls.Add(this.lblPlayerHandValue);
             this.Controls.Add(this.lblDealerHandValue);
             this.Controls.Add(this.lblPlayer);
@@ -298,6 +363,12 @@
         private System.Windows.Forms.Label lblPlayer;
         private System.Windows.Forms.Label lblDealerHandValue;
         private System.Windows.Forms.Label lblPlayerHandValue;
+        private System.Windows.Forms.ToolStripMenuItem hitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem standToolStripMenuItem;
+        private System.Windows.Forms.Button btnHit;
+        private System.Windows.Forms.Button btnStand;
+        private System.Windows.Forms.TextBox txtBet;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- ComeauBlackJack/ComeauBlackJack/BlackJackTable.cs
+++ ComeauBlackJack/ComeauBlackJack/BlackJackTable.cs
@@ -12,13 +12,38 @@ namespace ComeauBlackJack
 {
     public partial class BlackJackTable : Form
     {
+        // Enumeration to indicate player with
+        // higher score or winning hand.
+        enum HighPlayer
+        {
+            Undetermined,
+            Dealer,
+            DealerWin,
+            DealerBlackJack,
+            Player,
+            PlayerWin,
+            PlayerBlackJack,
+            Push
+        }
+
+        // Enumeration to indicate the stage
+        // of each hand.
+        enum PlayerTurn
+        {
+            Player,
+            Dealer,
+            Close
+        }
+
         // Maintain current player and deck for access.
         Player tablePlayer = new Player("Guest", 1000, false);
         Player dealerPlayer = new Player("Dealer", 1000, true);
         ActiveDeck currentDeck;
+        PlayerTurn Turn = PlayerTurn.Player;
 
         const string BLUEBACK = "Backface_Blue.bmp";
         const string REDBACK = "Backface_Red.bmp";
+        const int BLACKJACK = 21;
 
         public BlackJackTable()
         {
@@ -40,7 +65,7 @@ namespace ComeauBlackJack
             try
             {
                 lblPlayerName.Text = "Playing as: " + tablePlayer.PlayerName;
-                lblPlayer.Text = tablePlayer.PlayerName;
+                lblPlayer.Text = tablePlayer.PlayerName + ": ";
                 lblScore.Text = "Current Bank: " + tablePlayer.BankAmount.ToString("$###,###.##");
             }
             catch (Exception ex)
@@ -57,15 +82,20 @@ namespace ComeauBlackJack
             int rowCount = 1;
             int handValue = SpecificPlayer.GetHandValue();
 
+            // Refresh the display of a player's hand and update the
+            // total value on the screen.
+
             try
             {
                 Panel focusPanel;
 
+                // Determine which panel to populate with cards.
                 if (SpecificPlayer.Dealer)
                     focusPanel = panelDealer;
                 else
                     focusPanel = panelPlayer;
 
+                // Clear the cards from the panel.
                 focusPanel.Controls.Clear();
 
                 foreach (PlayingCard card in SpecificPlayer.PlayerHand)
@@ -74,16 +104,18 @@ namespace ComeauBlackJack
                     // apply the correct settings and add to the form
                     // controls.
                     PictureBox newCard = new PictureBox();
-                    
+
                     newCard.Location = new Point(leftPos, topPos);
                     newCard.Size = new Size(50, 70);
                     newCard.SizeMode = PictureBoxSizeMode.AutoSize;
 
+                    // Substitute back images for facedown cards.
                     if (card.FaceDown)
                         newCard.Image = SourceDeck.Images[REDBACK];
                     else
                         newCard.Image = SourceDeck.Images[card.CardName];
-                    
+
+                    // Add card to panel.
                     focusPanel.Controls.Add(newCard);
                     newCard.BringToFront();
 
@@ -103,9 +135,9 @@ namespace ComeauBlackJack
 
                 // Update hand values.
                 if (SpecificPlayer.Dealer)
-                    lblDealerHandValue.Text = handValue.ToString();
+                    lblDealerHandValue.Text = "Total: " + handValue.ToString();
                 else
-                    lblPlayerHandValue.Text = handValue.ToString();
+                    lblPlayerHandValue.Text = "Total: " + handValue.ToString();
             }
             catch (Exception ex)
             {
@@ -122,10 +154,13 @@ namespace ComeauBlackJack
 
             try
             {
+                // Clear the table and start a new game.
+                ClearTable();
+
                 // Open the player selection dialog.
-                using(PlayerLoad plForm = new PlayerLoad())
+                using (PlayerLoad plForm = new PlayerLoad())
                 {
-                    if(plForm.ShowDialog(this) == DialogResult.OK)
+                    if (plForm.ShowDialog(this) == DialogResult.OK)
                     {
                         // Get the dialog values.
                         newPlayerName = plForm.txtPlayerName.Text;
@@ -134,14 +169,14 @@ namespace ComeauBlackJack
 
                         // Give precedence to a new player name.
                         if (newPlayerName.Length > 0)
-                        { 
+                        {
                             tablePlayer.PlayerName = newPlayerName;
                             tablePlayer.BankAmount = 1000;
                         }
                         else if (loadPlayerName.Length > 0)
                         {
                             tablePlayer.PlayerName = loadPlayerName;
-                            if(getScore)
+                            if (getScore)
                             {
                                 tablePlayer.BankAmount = plForm.PlayerScore;
                             }
@@ -158,7 +193,7 @@ namespace ComeauBlackJack
             {
                 MessageBox.Show(ex.Message, "Error ...");
             }
-            
+
         }
 
         private void exitToolStripMenuItem_Click(object sender, EventArgs e)
@@ -166,16 +201,51 @@ namespace ComeauBlackJack
             Application.Exit();
         }
 
-        private void DealHand_Click(object sender, EventArgs e)
+
+        private void ClearPlayerHands()
         {
             try
             {
                 // Clear player hands.
-
                 currentDeck.Discard(dealerPlayer.PlayerHand);
                 currentDeck.Discard(tablePlayer.PlayerHand);
                 dealerPlayer.PlayerHand.Clear();
                 tablePlayer.PlayerHand.Clear();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error ...");
+            }
+
+        }
+
+        private void ClearTable()
+        {
+
+            try
+            {
+                // Clear the board
+                ClearPlayerHands();
+                currentDeck.ShuffleDeck(SourceDeck);
+                panelDealer.Controls.Clear();
+                panelPlayer.Controls.Clear();
+                lblDealerHandValue.Text = "";
+                lblPlayerHandValue.Text = "";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error ...");
+            }
+        }
+
+        private void DealHand_Click(object sender, EventArgs e)
+        {
+            double playerBet = 0;
+
+            try
+            {
+                ClearPlayerHands();
+                UpdatePlayerStats();
 
                 // If the current deck is getting low, shuffle.
                 if (currentDeck.RemainingCards.Count < 18)
@@ -183,23 +253,214 @@ namespace ComeauBlackJack
                     currentDeck.ShuffleDeck(true, true);
                 }
 
-                // Deal two cards face up to the player.
-                currentDeck.DealCards(2, tablePlayer, false);
+                if (double.TryParse(txtBet.Text, out playerBet))
+                {
+                    txtBet.ReadOnly = true;
 
-                // Deal two cards to the dealer, one facedown
-                currentDeck.DealCards(1, dealerPlayer, true);
-                currentDeck.DealCards(1, dealerPlayer, false);
+                    // Deal two cards face up to the player.
+                    currentDeck.DealCards(2, tablePlayer, false);
 
-                // Display hands.
-                DisplayHand(dealerPlayer);
-                DisplayHand(tablePlayer);
+                    // Deal two cards to the dealer, one facedown
+                    currentDeck.DealCards(1, dealerPlayer, true);
+                    currentDeck.DealCards(1, dealerPlayer, false);
 
+                    // Display hands.
+                    DisplayHand(dealerPlayer);
+                    DisplayHand(tablePlayer);
+
+                    // Turn the play over to the player at the table.
+                    Turn = PlayerTurn.Player;
+
+                    // Look for immediate Blackjack
+                    HandProgress(EvaluateHands());
+                }
+                else
+                {
+                    MessageBox.Show("Please enter a numeric value for the bet.");
+                    txtBet.Text = "";
+                }
             }
             catch (Exception ex)
             {
                 throw ex;
             }
         }
+
+        private void HitButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Deal a card to the player at the table and redipslay hand.
+                currentDeck.DealCards(1, tablePlayer, false);
+                DisplayHand(tablePlayer);
+                HandProgress(EvaluateHands());
+
+                // If the dealer won, display the hand.
+                if (Turn == PlayerTurn.Close)
+                    DisplayHand(dealerPlayer);
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message, "Error ...");
+            }
+        }
+
+        private void StandButton_Click(object sender, EventArgs e)
+        {
+            int dealerHandValue;
+
+            try
+            {
+                // Turn it over to the dealer to complete the hand.
+                Turn = PlayerTurn.Dealer;
+
+                // Turn the dealer's cards face up.
+                foreach (PlayingCard pCard in dealerPlayer.PlayerHand)
+                    pCard.FaceDown = false;
+
+                // Get the new hand value.
+                dealerHandValue = dealerPlayer.GetHandValue();
+
+                // If the dealer is dealt 17 or more, stand.
+                if (dealerHandValue > 16)
+                    HandProgress(EvaluateHands());
+                else
+                {
+                    while (dealerPlayer.GetHandValue() < 17)
+                    {
+                        currentDeck.DealCards(1, dealerPlayer, false);
+                        HandProgress(EvaluateHands());
+                    }
+                }
+
+                DisplayHand(dealerPlayer);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error ...");
+            }
+        }
+
+        private void ProcessBet(Player BettingPlayer, double AdjAmount)
+        {
+            try
+            {
+                BettingPlayer.BankAmount += AdjAmount;
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message, "Error ...");
+            }
+        }
+
+        private void HandProgress(HighPlayer status)
+        {
+            double betAmount;
+
+            try
+            {
+                betAmount = double.Parse(txtBet.Text);
+
+                switch (status)
+                {
+                    case HighPlayer.DealerWin:
+                        MessageBox.Show("Dealer wins.");
+                        ProcessBet(tablePlayer, (betAmount * -1));
+                        break;
+                    case HighPlayer.DealerBlackJack:
+                        MessageBox.Show("Dealer has Blackjack.");
+                        ProcessBet(tablePlayer, (betAmount * -1));
+                        break;
+                    case HighPlayer.PlayerWin:
+                        MessageBox.Show("You win!");
+                        ProcessBet(tablePlayer, (betAmount));
+                        break;
+                    case HighPlayer.PlayerBlackJack:
+                        MessageBox.Show("BLACKJACK!");
+                        ProcessBet(tablePlayer, (betAmount));
+                        break;
+                    case HighPlayer.Push:
+                        if (Turn == PlayerTurn.Close)
+                            MessageBox.Show("You and the Dealer push ... sorry.");
+                        break;
+                    default:
+                        break;
+                }                  
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error ...");
+            }
+        }
+
+        private HighPlayer EvaluateHands()
+        {
+
+            HighPlayer result = HighPlayer.Undetermined;
+            int playerHand = tablePlayer.GetHandValue();
+            int dealerHand = dealerPlayer.GetHandValue();
+
+            try
+            {
+                // Look for a push.
+                if (playerHand == dealerHand)
+                {
+                    result = HighPlayer.Push;
+
+                    if (playerHand >= BLACKJACK)
+                        Turn = PlayerTurn.Close;
+                }
+                    
+
+                // Look for a bust
+                if (result == HighPlayer.Undetermined)
+                {
+                    if (playerHand > BLACKJACK && dealerHand < BLACKJACK)
+                        result = HighPlayer.DealerWin;
+                    else if (dealerHand > BLACKJACK && playerHand < BLACKJACK)
+                        result = HighPlayer.PlayerWin;
+
+                    if (result != HighPlayer.Undetermined) Turn = PlayerTurn.Close;
+                }
+
+                // Look for a player with 21.
+
+                if (result == HighPlayer.Undetermined)
+                {
+                    if (playerHand == BLACKJACK || dealerHand == BLACKJACK)
+                    {
+                        Turn = PlayerTurn.Close;
+                        if (playerHand == BLACKJACK)
+                            result = HighPlayer.PlayerBlackJack;
+                        else
+                            result = HighPlayer.DealerBlackJack;
+                    }
+                }
+
+                // Look for a non-winning high player.
+                if (result == HighPlayer.Undetermined)
+                    result = (playerHand > dealerHand) ? HighPlayer.Player : HighPlayer.Dealer;
+
+                // The dealer must stand on 17 or greater.
+                if (Turn == PlayerTurn.Dealer && dealerHand > 16)
+                {
+                    result = (playerHand > dealerHand) ? HighPlayer.PlayerWin : HighPlayer.DealerWin;
+                    Turn = PlayerTurn.Close;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error ...");
+            }
+
+            return result;
+
+        }
+
 
 
     }
